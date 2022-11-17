f20a5ae7e826e3e28721f8ee4fbcc84be00a3d9b
GuiCampos18 <raiane.sana75@gmail.com>
2022-11-17 09:24:11 -0300

    Aula de Javascript"

---
+++ site.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var data = require('./primeiromodulo');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Aula de Javascript</h1>');
+    res.write('<h2>Servidor criado!</h2>');
+    res.write('Endereco acessado: '+req.url+'<br>');
+    res.write(data.myDateTime());
+    res.end();
+    
+}).listen(8080);
